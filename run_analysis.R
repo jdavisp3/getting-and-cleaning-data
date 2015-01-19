9de75cc2612011d03c47fc81578c34ddb19a3cb3
Dave Peticolas <dpeticolas@counsyl.com>
2015-01-18 19:09:44 -0800

    Activity labels.

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,12 @@
 # Clean the Smartphone data
+
+data_dir <- 'UCI HAR Dataset'
+features_file <- file.path(data_dir, 'features.txt')
+
+get_activities <- function() {
+  activity_labels_file <- file.path(data_dir, 'activity_labels.txt')
+  activity_data <- read.csv(activity_labels_file, header=FALSE, sep=' ')
+  factor(activity_data$V1, labels=activity_data$V2)
+}
+
+activities <- get_activities()
